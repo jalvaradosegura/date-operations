79bdfc4eb9f62756c20fa2123ee69fa1aaa834e7
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2023-05-10 02:11:01 -0400

    ✨ Add "started" functions
    These functions calculate the amount of months or years started between
    2 dates. E.g. between the last day of a month and the first day of the
    next month, 1 month started.

--- date_operations/main.py
+++ date_operations/main.py
@@ -16,8 +16,28 @@ def months_between(date_1: str, date_2: str) -> int:
     return abs(delta.months + (12 * delta.years))
 
 
+def months_started_between(date_1: str, date_2: str) -> int:
+    months_started = (
+        datetime.strptime(date_1, "%Y-%m-%d").month
+        - datetime.strptime(date_2, "%Y-%m-%d").month
+    )
+    years_started = (
+        datetime.strptime(date_1, "%Y-%m-%d").year
+        - datetime.strptime(date_2, "%Y-%m-%d").year
+    )
+    return abs(years_started * 12 + months_started)
+
+
 def years_between(date_1: str, date_2: str) -> int:
     delta = relativedelta(
         datetime.strptime(date_1, "%Y-%m-%d"), datetime.strptime(date_2, "%Y-%m-%d")
     )
     return abs(delta.years)
+
+
+def years_started_between(date_1: str, date_2: str) -> int:
+    years_started = (
+        datetime.strptime(date_1, "%Y-%m-%d").year
+        - datetime.strptime(date_2, "%Y-%m-%d").year
+    )
+    return abs(years_started)
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,13 @@ from typing import Tuple
 
 import pytest
 
-from date_operations.main import days_between, months_between, years_between
+from date_operations.main import (
+    days_between,
+    months_between,
+    months_started_between,
+    years_between,
+    years_started_between,
+)
 
 
 @pytest.mark.parametrize(
@@ -26,6 +32,7 @@ def test_days_between(dates: Tuple[str, str], result: int):
         [("2023-02-01", "2023-01-01"), 1],
         [("2023-01-01", "2023-01-01"), 0],
         [("2023-01-01", "2024-01-01"), 12],
+        [("2023-01-31", "2023-02-01"), 0],
     ],
 )
 def test_months_between(dates: Tuple[str, str], result: int):
@@ -36,12 +43,48 @@ def test_months_between(dates: Tuple[str, str], result: int):
 @pytest.mark.parametrize(
     "dates, result",
     [
+        [("2023-01-31", "2023-02-01"), 1],
+        [("2023-02-01", "2023-01-01"), 1],
+        [("2023-12-31", "2024-01-01"), 1],
+        [("2024-01-01", "2023-12-31"), 1],
+        [("2023-01-01", "2023-02-01"), 1],
+        [("2023-02-01", "2023-01-01"), 1],
+        [("2023-01-01", "2023-01-01"), 0],
+        [("2023-01-01", "2024-01-01"), 12],
+    ],
+)
+def test_months_started_between(dates: Tuple[str, str], result: int):
+    date_1, date_2 = dates
+    assert months_started_between(date_1, date_2) == result
+
+
+@pytest.mark.parametrize(
+    "dates, result",
+    [
+        [("2023-01-01", "2024-01-01"), 1],
+        [("2023-01-01", "2022-01-01"), 1],
+        [("2023-01-01", "2023-01-01"), 0],
+        [("2023-01-01", "2030-01-01"), 7],
+        [("2023-12-31", "2024-01-01"), 0],
+    ],
+)
+def test_years_between(dates: Tuple[str, str], result: int):
+    date_1, date_2 = dates
+    assert years_between(date_1, date_2) == result
+
+
+@pytest.mark.parametrize(
+    "dates, result",
+    [
+        [("2023-12-31", "2024-01-01"), 1],
+        [("2023-01-01", "2022-12-31"), 1],
+        [("2023-01-01", "2030-01-01"), 7],
         [("2023-01-01", "2024-01-01"), 1],
         [("2023-01-01", "2022-01-01"), 1],
         [("2023-01-01", "2023-01-01"), 0],
         [("2023-01-01", "2030-01-01"), 7],
     ],
 )
-def test_years_between(dates: Tuple[str, str], result: int):
+def test_years_started_between(dates: Tuple[str, str], result: int):
     date_1, date_2 = dates
-    assert years_between(date_1, date_2) == result
+    assert years_started_between(date_1, date_2) == result
