b027de088aeb19ec95893fbf8089ff080f4ec393
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2023-05-13 17:56:04 -0400

    ✨ Turn decorator into a simple function
    Instead of using a decorator, now there is a function that tries to
    cast a date into a `datetime.datetime` object by guessing the format
    or using the format defined by the user.

--- date_operations/main.py
+++ date_operations/main.py
@@ -1,115 +1,90 @@
 from datetime import datetime
-from functools import wraps
-from typing import Callable, List, Optional
+from typing import List, Optional
 
 from dateutil.relativedelta import relativedelta
 
 from .settings import date_format_list
 
 
-def guess_date_format(func: Callable[[str, str, str, str, Optional[List[str]]], int]):
-    @wraps(func)
-    def _wrapper(
-        date_1: str,
-        date_2: str,
-        format_1: Optional[str] = None,
-        format_2: Optional[str] = None,
-        extra_formats: Optional[List[str]] = None,
-    ) -> int:
-        if format_1 is None:
-            format_1 = _guess_date_format(date_1, extra_formats)
-
-            if format_1 is None:
-                raise ValueError(
-                    f"Couldn't guess the date format for date_1: {date_1}."
-                )
-
-        if format_2 is None:
-            format_2 = _guess_date_format(date_2, extra_formats)
-
-            if format_2 is None:
-                raise ValueError(
-                    f"Couldn't guess the date format for date_2: {date_2}."
-                )
-
-        return func(date_1, date_2, format_1, format_2, extra_formats)
-
-    return _wrapper
-
-
-@guess_date_format
 def days_between(
     date_1: str,
     date_2: str,
-    format_1: str,
-    format_2: str,
+    format_1: Optional[str] = None,
+    format_2: Optional[str] = None,
     extra_formats: Optional[List[str]] = None,
 ) -> int:
     return abs(
-        datetime.strptime(date_1, format_1) - datetime.strptime(date_2, format_2)
+        _to_datetime(date_1, format_1, extra_formats)
+        - _to_datetime(date_2, format_2, extra_formats)
     ).days
 
 
-@guess_date_format
 def months_between(
     date_1: str,
     date_2: str,
-    format_1: str,
-    format_2: str,
+    format_1: Optional[str] = None,
+    format_2: Optional[str] = None,
     extra_formats: Optional[List[str]] = None,
 ) -> int:
     delta = relativedelta(
-        datetime.strptime(date_1, format_1), datetime.strptime(date_2, format_2)
+        _to_datetime(date_1, format_1, extra_formats),
+        _to_datetime(date_2, format_2, extra_formats),
     )
     return abs(delta.months + (12 * delta.years))
 
 
-@guess_date_format
 def months_started_between(
     date_1: str,
     date_2: str,
-    format_1: str,
-    format_2: str,
+    format_1: Optional[str] = None,
+    format_2: Optional[str] = None,
     extra_formats: Optional[List[str]] = None,
 ) -> int:
-    months_started = (
-        datetime.strptime(date_1, format_1).month
-        - datetime.strptime(date_2, format_2).month
-    )
-    years_started = (
-        datetime.strptime(date_1, format_1).year
-        - datetime.strptime(date_2, format_2).year
-    )
+    ddate_1 = _to_datetime(date_1, format_1, extra_formats)
+    ddate_2 = _to_datetime(date_2, format_2, extra_formats)
+    months_started = ddate_1.month - ddate_2.month
+    years_started = ddate_1.year - ddate_2.year
     return abs(years_started * 12 + months_started)
 
 
-@guess_date_format
 def years_between(
     date_1: str,
     date_2: str,
-    format_1: str,
-    format_2: str,
+    format_1: Optional[str] = None,
+    format_2: Optional[str] = None,
     extra_formats: Optional[List[str]] = None,
 ) -> int:
     delta = relativedelta(
-        datetime.strptime(date_1, format_1), datetime.strptime(date_2, format_2)
+        _to_datetime(date_1, format_1, extra_formats),
+        _to_datetime(date_2, format_2, extra_formats),
     )
     return abs(delta.years)
 
 
-@guess_date_format
 def years_started_between(
     date_1: str,
     date_2: str,
-    format_1: str,
-    format_2: str,
+    format_1: Optional[str] = None,
+    format_2: Optional[str] = None,
     extra_formats: Optional[List[str]] = None,
 ) -> int:
     years_started = (
-        datetime.strptime(date_1, format_1).year
-        - datetime.strptime(date_2, format_2).year
+        _to_datetime(date_1, format_1, extra_formats).year
+        - _to_datetime(date_2, format_2, extra_formats).year
     )
     return abs(years_started)
+
+
+def _to_datetime(
+    date: str, _format: Optional[str], extra_formats: Optional[List[str]]
+) -> datetime:
+    if _format is None:
+        _format = _guess_date_format(date, _format)
+
+    if _format is None:
+        raise ValueError(f"Couldn't guess the date format for date: {date}.")
+
+    return datetime.strptime(date, _format)
 
 
 def _guess_date_format(
